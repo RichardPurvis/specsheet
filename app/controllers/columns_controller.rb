17b75ae0cefb5b2dd8f00dfd878b80e061b9eefe
RichardPurvis <steelthunderfalcon@gmail.com>
2015-03-19 18:01:59 -0400

    not a hash anymore

--- app/controllers/columns_controller.rb
+++ app/controllers/columns_controller.rb
@@ -3,8 +3,25 @@ class ColumnsController < ApplicationController
   end
 
   def create
+    @sheet = Sheet.find(params[:sheet_id])
+    @columns = @sheet.columns
+
+    @column = Column.new(column_params)
+    @column.sheet = @sheet
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+
   end
 
   def destroy
+  end
+
+  private
+
+  def column_params
+    params.require(:column).permit(:title)
   end
 end
